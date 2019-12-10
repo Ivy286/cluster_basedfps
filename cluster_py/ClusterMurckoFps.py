5786c48cfa9ed552a50520721e1396a05b3b2c03
xh <983187209@qq.com>
2019-12-10 19:09:49 +0800

    modify

--- cluster_py/ClusterMurckoFps.py
+++ cluster_py/ClusterMurckoFps.py
@@ -1,5 +1,6 @@
-from cluster import *
+from cluster_m import *
 from murcko_scaffold import *
+import pandas as pd
 import optparse
 import sys
 import os
@@ -27,12 +28,13 @@ def getOptions():
     parser.add_option('--out', dest='output', help='output sdf file or csv file', default='')
     options, args = parser.parse_args()
 
+    print(options.input1, options.input2, options.output)
     # options.input = DATAPATH
     # options.output = os.path.join(SAVEDPATH, options.output)
-    if options.input1 == '' or options.input2 or options.output == '':
-        parser.print_help()
-        print("No input1 or input2 or output is provided")
-        sys.exit(1)
+    # if options.input1 == '' or options.input2 or options.output == '':
+    #     parser.print_help()
+    #     print("No input1 or input2 or output is provided")
+    #     sys.exit(1)
 
     return options
 
@@ -55,31 +57,36 @@ def main():
     print('input file reading...')
     smis_dataprocess(merge_file(options.input1, options.input2), smi_col=0, id_col=1)
 
+    merge_df = pd.read_csv('clustered_based_scaffold.csv')
+    merge_df_new = merge_df[merge_df['num_count'] >= 5]
+
+    df_tmp = pd.DataFrame()
+    for num, i in enumerate(set(merge_df_new['cluster_label'].tolist())):
+        df_item = merge_df[merge_df['cluster_label'] == int(i)]
+        input_parse = ChemParse(df_item)
+        print('cluster%d fingerprint calculating...' % i)
+        input_parse.get_fps(options.fp, options.radius)
+        print('cluster%d clustering...' % i)
+        fp_cluster = Fingerprint_Cluster(input_parse.fps)
+        fp_cluster.distance_matrix()
+        fp_cluster.cluster_dict(options.algorithm, options.cutoff, options.Murtype, options.nclusts)
+        print('done, output of cluster{} to {}'.format(i, options.output))
+        if num == 0:
+            out_df, index = input_parse.clusterOutput(options.output, fp_cluster.cdict)
+            df_tmp = out_df
+        else:
+            out_df, index = input_parse.clusterOutput(options.output, fp_cluster.cdict, index_=int(len(fp_cluster.clustdict)))
+            df_tmp = pd.concat([df_tmp, out_df], axis=0)
+
+        df_final = pd.concat([df_tmp, merge_df[merge_df['num_count'] < 5]], axis=0)
+        df_final = pd.merge(df_final, merge_df, on='ID')  #.drop(['drug_id', ''],axis=1)
+        # for c in fp_cluster.clustdict:
+        #     print("cluster%s: %s" % (str(c), str(fp_cluster.clustdict[c])))
+        df_final.to_csv(options.output, index=False)
 
 
-    input_parse = ChemParse('clustered_based_scaffold.csv')
-    if vars(options)['smi_col'] is None or vars(options)['id_col'] is None:
-        input_parse.input_reader()
-    else:
-        input_parse.input_reader(vars(options)['smi_col'], vars(options)['id_col'])
-
-
-
-
-    print('fingerprint calculating...')
-    input_parse.get_fps(options.fp, options.radius)
-    print('clustering...')
-    fp_cluster = Fingerprint_Cluster(input_parse.fps)
-    fp_cluster.distance_matrix()
-    fp_cluster.cluster_dict(options.algorithm, options.cutoff, options.Murtype, options.nclusts)
-    print('done, output to %s' % options.output)
-    input_parse.clusterOutput(options.output, fp_cluster.cdict)
-    for c in fp_cluster.clustdict:
-        print("cluster%s: %s" % (str(c), str(fp_cluster.clustdict[c])))
-    
-    
 if __name__ == "__main__":
-    os.chdir('./')
-    df = merge_file('r2_select_top1w.csv', 'r2_select_top1w_docking_result_processed.csv')
-    smis_dataprocess(df, smi_col=0, id_col=1)
+    # os.chdir('./')
+    # df = merge_file('r2_select_top1w.csv', 'r2_select_top1w_docking_result_processed.csv')
+    # smis_dataprocess(df, smi_col=0, id_col=1)
     main()
